004f769755f4c4bbe5c1e6c030d77fb890e6042d
lhernala <alberto9632016@outlook.com>
2021-11-18 18:03:41 -0600

    Final Final

---
+++ src/main/java/com/example/practica/spring611/entity/Teacher2.java
@@ -0,0 +1,27 @@
+package com.example.practica.spring611.entity;
+
+public class Teacher2 {
+    private String nombre;
+    private String direccion;
+
+    public Teacher2(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
